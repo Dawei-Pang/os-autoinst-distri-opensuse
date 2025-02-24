475985667d28978e9c718a783465ab772080ecd9
Dawei Pang <dawei.pang@suse.com>
2025-02-24 20:53:45 +0800

    continue to try

--- tests/kernel_performance/hanaperf_postinstall.pm
+++ tests/kernel_performance/hanaperf_postinstall.pm
@@ -17,6 +17,7 @@ sub run {
     my $self = shift;
 
     select_console 'sol', await_console => 0;
+    select_serial_terminal;
 
     assert_script_run 'nmcli connection show';
     assert_script_run 'rm -f /etc/NetworkManager/system-connections/default_connection.nmconnection';
