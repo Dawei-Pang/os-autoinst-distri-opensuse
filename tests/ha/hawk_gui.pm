b5ba52f0dad195c6490afd582cde6df0c263f426
Dawei Pang <dawei.pang@suse.com>
2026-03-25 12:31:12 +0800

    update 16_0

--- tests/ha/hawk_gui.pm
+++ tests/ha/hawk_gui.pm
@@ -42,7 +42,7 @@ sub run {
     # TODO: Use another namespace using team group name
     # Docker image source in https://github.com/ricardobranco777/hawk_test
     # It will be eventually moved to https://github.com/ClusterLabs/hawk/e2e_test
-    my $image = "registry.opensuse.org/home/dawei_pang/branches/devel/openqa/ci/tooling/containers_15_4/hawk_test:latest";
+    my $image = "registry.opensuse.org/home/dawei_pang/branches/devel/openqa/ci/tooling/containers_16_0/hawk_test:latest";
     assert_script_run("env DOCKER_CONTENT_TRUST=1 podman pull $image", 240);
 
     # Rest of the test needs to be performed on the x11 console, but with the
